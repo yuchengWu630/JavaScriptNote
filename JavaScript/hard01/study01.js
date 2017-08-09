badd6ce014aa1715af55d1c4b72fe69efb68590d
yuchengWu <243370725@qq.com>
2017-08-09 13:41:47 +0800

    难题

--- JavaScript/hard01/study01.js
+++ JavaScript/hard01/study01.js
@@ -1,25 +1,28 @@
-function C(){    this.yuchengWu = [];}
+function C(){
+        this.yuchengWu = [];
+};
 var yuchengWu = new C();
+
 C.prototype.bind = function(key,fun){
         var tasks = this.yuchengWu[key] || [];    
         tasks.push(fun);    
         this.yuchengWu[key] = tasks;
-    };
+};
 C.prototype.exec = function(key,fun,param){
         var args = Array.prototype.slice.apply(arguments).slice(2);    
         var tasks = this.yuchengWu[key];    
         tasks = [];    
         tasks.push(fun);    
         tasks[0].apply(this,args);
-    };
+};
 C.prototype.execAll = function(key,param){
         var tasks = this.yuchengWu[key];    
         var args = Array.prototype.slice.apply(arguments).slice(1);    
         if(!Array.isArray(tasks)) return;    
-        tasks.forEach(function(fun){            
-            fun.apply(this,args)  
-        })
-    };
+        tasks.forEach(function(fun){
+            fun.apply(this,args)
+        });
+};
 C.prototype.del = function(key ,fun){    
     var tasks = this.yuchengWu[key];    
     var method = fun.toString();    
@@ -32,8 +35,8 @@ C.prototype.del = function(key ,fun){
     };   
     if(state == 0){
                 console.log("没找到对应函数")    
-            };  
-        };
+    };  
+};
 C.prototype.delAll = function (key) {
         this.yuchengWu[key]=[]
-    };
+};
